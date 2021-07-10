e9762e2c8150258426862c8d0b43f3c5bbfcd229
coryphoenixxx <coryphoenixxx@gmail.com>
2021-07-10 07:15:41 +0700

    *implement getting russian version of comic *implement getting explanation of comic

--- bot.py
+++ bot.py
@@ -5,49 +5,59 @@ from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKe
 from aiogram.dispatcher.filters import Text
 from aiogram import Bot, Dispatcher, executor
 
-from parser_ import get_comic, BASE_URL
+from parser_ import get_comic, BASE_URL, get_rus_version, get_eng_explanation
 from config import Config
 
 loop = asyncio.get_event_loop()
 bot = Bot(Config.API_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher(bot, loop=loop)
 
-LAST = 2485
+
+LAST = 2486
 CUR_NUM = 0
 
 
 def get_keyboard():
     buttons = [
-        InlineKeyboardButton(text='<<First', callback_data='nav_first'),
+        InlineKeyboardButton(text='|<', callback_data='nav_first'),
         InlineKeyboardButton(text='<Prev', callback_data='nav_prev'),
-        InlineKeyboardButton(text='Random', callback_data='nav_random'),
+        InlineKeyboardButton(text='Rand', callback_data='nav_random'),
         InlineKeyboardButton(text='Next>', callback_data='nav_next'),
-        InlineKeyboardButton(text='Last>>', callback_data='nav_last'),
-        # InlineKeyboardButton(text='Favorite', callback_data=''),
-        # InlineKeyboardButton(text='Subscribe>>', callback_data='')
+        InlineKeyboardButton(text='>|', callback_data='nav_last'),
+        InlineKeyboardButton(text='Bookmark\u2606', callback_data='fav'),
+        InlineKeyboardButton(text='Explain', callback_data='explain'),
+        InlineKeyboardButton(text='Rus', callback_data='rus'),
     ]
 
-    keyboard = InlineKeyboardMarkup(row_width=5,)
+    keyboard = InlineKeyboardMarkup(row_width=5)
     keyboard.add(*buttons)
     return keyboard
 
 
-async def notify_admin(dp):
+async def notify_admin(dp: Dispatcher):
+    await dp.skip_updates()
     await bot.send_message(chat_id=Config.ADMIN_ID, text="...Bot in Your Power, My Lord...")
 
 
 @dp.message_handler(commands=['start', 'help'])
 async def hello(message: Message):
-    await message.answer('Hi! I\'m TeleXKCDbot')
-    await asyncio.sleep(1)
-    await message.answer('Write the xkcd-comic\'s number')
+    await message.answer("Hey! I'm xkcd bot.\n")
+    await asyncio.sleep(1.5)
+    await message.answer("Here is my list of commands:\n"
+                         "- Type in the comic number and I'll find it for you!\n"
+                         "Type nothing and I'll send you the latest xkcd.\n"
+                         "/read - read-mode\n"
+                         "/subscribe - I'll notify you whenever a new xkcd is released.\n"
+                         "/unsubscribe - I'll stop notifying you when new xkcd comics are released.\n"
+                         "/fav - I'll send you your bookmarks.\n"
+                         "/random - I'll send you a random xkcd.")
 
 
 async def ext_answer(message: Message, data: dict):
     num, title, img_url, comment = data.values()
 
     await message.answer(text=f"<b>{str(num)}. \"<a href='{BASE_URL + str(num)}'>{title}</a>\"</b>",
-                         disable_web_page_preview=True)
+                         disable_web_page_preview=True, disable_notification=True)
 
     if img_url.endswith(('.png', '.jpg')):
         await message.answer_photo(photo=img_url)
@@ -56,12 +66,14 @@ async def ext_answer(message: Message, data: dict):
     else:
         await message.answer("Can\'t upload pic/gif...\nUse browser to read comic.")
 
-    await message.answer(f'<i>{comment}</i>', reply_markup=get_keyboard())
+    await message.answer(f'<i>{comment}</i>', reply_markup=get_keyboard(), disable_notification=True)
 
 
 @dp.message_handler(commands="read")
 async def cmd_read(message: Message):
-    default_num = 1608  #1608, 1190, 1416 - defective
+    global CUR_NUM
+    default_num = 404  # 1608, 1190, 1416, 404, 1037, 1538, 1953, 1965(div) - defective
+    CUR_NUM = default_num
     img_data = get_comic(default_num)
     await ext_answer(message, img_data)
 
@@ -92,14 +104,34 @@ async def nav(call: CallbackQuery):
     await ext_answer(call.message, img_data)
 
 
+@dp.callback_query_handler(Text('rus'))
+async def rus_version(call: CallbackQuery):
+    global CUR_NUM
+    data = get_rus_version(CUR_NUM)
+    if data:
+        await ext_answer(call.message, data)
+    else:
+        await call.message.answer('There\'s no russian version...(((')
+
+
+@dp.callback_query_handler(Text('explain'))
+async def explanation(call: CallbackQuery):
+    global CUR_NUM
+    text = get_eng_explanation(CUR_NUM)
+    await call.message.answer(text)
+
+
 @dp.message_handler()
 async def echo(message: Message):
+    global CUR_NUM
     user_input = message.text
     if user_input.isdigit():
-        img_url = get_comic(user_input).get('img_url')
-        await message.answer_photo(photo=img_url)
+        num = int(user_input)
+        comic_data = get_comic(num)
+        CUR_NUM = num
+        await ext_answer(message, data=comic_data)
     else:
-        await message.answer('Please, write a number!')
+        await message.answer('Please, write a number or command!')
 
 
 if __name__ == "__main__":
--- parser_.py
+++ parser_.py
@@ -1,33 +1,59 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 
 
 BASE_URL = 'https://xkcd.com/'
-
+RUS_URL = "https://xkcd.ru/num/"
+EXPLAIN_URL = "https://www.explainxkcd.com"
 
 def get_comic(number):
-    url = BASE_URL + str(number)
-    content = requests.get(url).content
-    soup = BeautifulSoup(content, 'lxml')
-    img = soup.find('div', {'id': 'comic'}).find_next('img')
+    url = f"{BASE_URL}/{number}/info.0.json"
+    info = requests.get(url).json()
 
     data = {
         'num': number,
-        'title': soup.find('div', {'id': 'ctitle'}).text,
-        'img_url': 'https:' + img.get('src'),
-        'comment': img.get('title')
+        'title': info['title'],
+        'img_url': info['img'],
+        'comment': info['alt'],
     }
-    print(data)
-
-    # img_url_html = soup.find('div', {'id': 'comic'}).find_all_next('br')[1].next
-    # img_url_real = img_url_html.split()[-1]
-    # img = soup.find('div', {'id': 'comic'}).find_next('img')
-
     return data
 
 
-if __name__ == "__main__":
-    import random
+def get_rus_version(number):
 
-    num = random.randint(1, 2800)
-    get_comic(num)
+    content = requests.get(RUS_URL).content
+    soup = BeautifulSoup(content, 'lxml')
+    li = soup.find('ul', {'class': 'list'}).find_all('li', {'class': 'real'})
+    nums = []
+    for i in li:
+        nums.append(int(i.text))
+
+    if number in nums:
+        content = requests.get(f"https://xkcd.ru/{number}/").content
+        soup = BeautifulSoup(content, 'lxml')
+        img = soup.find('img', {'border': 0})
+        comment = soup.find('div', {'class': 'comics_text'}).text
+
+        data = {
+            'num': number,
+            'title': img['alt'],
+            'img_url': img['src'],
+            'comment': comment,
+        }
+
+        return data
+    else:
+        return None
+
+
+def get_eng_explanation(number):
+    url = f"{EXPLAIN_URL}/{number}/"
+    content = requests.get(url).content
+    soup = BeautifulSoup(content, 'lxml')
+    first_p = soup.find('span', {'id': 'Explanation'}).parent.findNext('p').text
+    return f"{first_p}\n<b>\"<a href='{url}'>...full text</a>\"</b>"
+
+
+if __name__ == "__main__":
+    print(get_eng_explanation(355))
