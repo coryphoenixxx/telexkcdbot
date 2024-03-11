7ee3556d3a44b3ecb658893efe9f9ff23419af30
coryphoenixxx <coryphoenixxx@gmail.com>
2024-03-12 06:12:15 +0700

    feature: Image processor now correctly detect animation

--- src/image_processor/main.py
+++ src/image_processor/main.py
@@ -3,7 +3,7 @@ from faststream.nats import JStream, NatsBroker, PullSub
 from PIL import Image
 from shared.messages import ImageProcessInMessage, ImageProcessOutMessage
 
-from image_processor.utils import convert_to_webp, create_thumbnail
+from image_processor.utils import convert_to_webp, create_thumbnail, is_animation
 
 broker = NatsBroker()
 app = FastStream(broker)
@@ -27,19 +27,22 @@ async def process_image_handler(msg: ImageProcessInMessage, logger: Logger):
     except FileNotFoundError:
         logger.error(f"Image file not found: {original_abs_path}")
     else:
-        converted_abs_path = convert_to_webp(img_obj, original_abs_path, logger)
-        thumbnail_abs_path = create_thumbnail(
-            img_obj,
-            converted_abs_path or original_abs_path,
-            logger,
-        )
+        try:
+            converted_abs_path = None
 
-        await broker.publish(
-            message=ImageProcessOutMessage(
-                image_id=msg.image_id,
-                converted_abs_path=converted_abs_path,
-                thumbnail_abs_path=thumbnail_abs_path,
-            ),
-            subject="internal.api.images.process.out",
-            stream="process_images_out_stream",
-        )
+            if not is_animation(img_obj):
+                converted_abs_path = convert_to_webp(img_obj, original_abs_path)
+
+            thumbnail_abs_path = create_thumbnail(img_obj, converted_abs_path or original_abs_path)
+        except Exception as err:
+            logger.exception(err, extra={"path": original_abs_path})
+        else:
+            await broker.publish(
+                message=ImageProcessOutMessage(
+                    image_id=msg.image_id,
+                    converted_abs_path=converted_abs_path,
+                    thumbnail_abs_path=thumbnail_abs_path,
+                ),
+                subject="internal.api.images.process.out",
+                stream="process_images_out_stream",
+            )
--- src/image_processor/utils.py
+++ src/image_processor/utils.py
@@ -1,8 +1,7 @@
 import warnings
 from pathlib import Path
 
-from faststream import Logger
-from PIL import Image
+from PIL import Image, ImageSequence
 from shared.types import ImageFormat
 
 THUMB_SIZE = (200, 200)
@@ -11,12 +10,19 @@ MAX_WEBP_SIZE = 16383
 warnings.simplefilter("ignore", Image.DecompressionBombWarning)
 
 
-def convert_to_webp(img_obj: Image, img_path: Path, logger: Logger) -> Path | None:
-    fmt = ImageFormat(img_path.suffix[1:])
+def is_animation(img_obj: Image) -> bool:
+    frames = 0
+    for _ in ImageSequence.Iterator(img_obj):
+        if frames > 1:
+            break
+        frames += 1
 
+    return frames > 1
+
+
+def convert_to_webp(img_obj: Image, img_path: Path) -> Path | None:
     if any(
         [
-            fmt == ImageFormat.GIF,
             (img_obj.height * img_obj.width) > Image.MAX_IMAGE_PIXELS,
             img_obj.width > MAX_WEBP_SIZE,
             img_obj.height > MAX_WEBP_SIZE,
@@ -28,32 +34,26 @@ def convert_to_webp(img_obj: Image, img_path: Path, logger: Logger) -> Path | No
         img_path.stem.replace("_original", "_converted"),
     ).with_suffix(".webp")
 
-    try:
-        img_obj.save(
-            fp=converted_abs_path,
-            format="webp",
-            lossless=fmt == ImageFormat.PNG,
-            quality=85 if fmt != ImageFormat.PNG else 100,
-            optimize=True,
-        )
-    except Exception as err:
-        logger.error(err)
-        logger.error(img_path)
-    else:
-        return converted_abs_path
+    fmt = ImageFormat(img_path.suffix[1:])
+
+    img_obj.save(
+        fp=converted_abs_path,
+        format="webp",
+        lossless=fmt == ImageFormat.PNG,
+        quality=85 if fmt != ImageFormat.PNG else 100,
+        optimize=True,
+    )
+
+    return converted_abs_path
 
 
-def create_thumbnail(img_obj: Image, img_path: Path, logger: Logger) -> Path | None:
+def create_thumbnail(img_obj: Image, img_path: Path) -> Path:
     old_name_parts = img_path.stem.split("_")
-    new_name = "_".join(old_name_parts[:-2]) + f"_{THUMB_SIZE[0]}x{THUMB_SIZE[1]}" + "_thumb"
+    new_name = "_".join(old_name_parts[:-2]) + f"_{THUMB_SIZE[0]}x{THUMB_SIZE[1]}_thumb"
 
     thumbnail_abs_path = img_path.with_name(new_name + img_path.suffix)
 
-    try:
-        img_obj.thumbnail(THUMB_SIZE)
-        img_obj.save(thumbnail_abs_path)
-    except Exception as err:
-        logger.error(err)
-        logger.error(img_path)
-    else:
-        return thumbnail_abs_path
+    img_obj.thumbnail(THUMB_SIZE)
+    img_obj.save(thumbnail_abs_path)
+
+    return thumbnail_abs_path
